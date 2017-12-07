8f9733172cda7891e7ab38f8e1861fc6fadcd179
t-nick <nickolaychuk.t@gmail.com>
2017-12-07 17:50:52 +0200

    add js

---
+++ Flat animation SVG/js/js.js
@@ -0,0 +1,9 @@
+$(document).ready($(".room").hover (
+	function () {
+		$(".description").html($(this).attr('desc-data'));
+		$(".description").fadeIn();
+	},
+	function () {
+		$(".description").fadeOut(50);
+	}
+));
